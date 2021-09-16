01e4c06279c7e63a46f45eec7bf0d87f13bada5c
Jake-Fischer <jakefischer000@gmail.com>
2021-09-16 15:31:48 -0500

    Change to clara's suggestions

--- quiz_project_refactored.py
+++ quiz_project_refactored.py
@@ -9,18 +9,18 @@ It will also work if more questions and answers are added to a topic already in 
 def main():
 
     question_bank = {
-        'art' : {
+        'art': {
             'Who painted the Mona Lisa?': 'Leonardo Da Vinci',
             'What precious stone is used to make the artist\'s pigment ultramarine?': 'Lapiz lazuli',
             'Anish Kapoor\'s bean-shaped Cloud Gate scuplture is a landmark of which city?' : 'Chicago',
-            },
+        },
         
-        'space' : {
+        'space': {
             'Which planet is closest to the sun?': 'Mercury',
             'Which planet spins in the opposite direction to all the others in the solar system?': 'Venus',
             'How many moons does Mars have?' : '2'
-            }
-            }
+        }
+    }
 
     # Print each question topic, prompt the user for topic, and convert the topic to lowercase
     print("Here are the available quiz topics:\n")
@@ -43,13 +43,13 @@ def main():
 def ask_questions(user_questions):
     score = 0
     for question in user_questions: # Loop through the dictionary of questions and answers
-        answer = input(question + ': ').lower() # Prompt the user with the question
-        if answer == user_questions[question].lower(): # If the answer is correct, add one to score, if not display the correct answer.
+        answer = input(question + ': ') # Prompt the user with the question
+        if answer.lower() == user_questions[question].lower(): # If the answer is correct, add one to score, if not display the correct answer.
             print('Correct!')
             score += 1
         else:
             print(f'Sorry, the correct answer is {user_questions[question]}')
-    print('End of quiz!')
+    print('End of topic questions!')
     return score
     
 main()
